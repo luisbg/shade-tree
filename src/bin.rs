ea02f0a634003bd7bcc56fa0f21178262a67386f
Luis de Bethencourt <luis@debethencourt.com>
2019-08-23 14:56:52 -0400

    Rust: add minifb dependency

--- src/bin.rs
+++ src/bin.rs
@@ -1,3 +1,29 @@
+extern crate minifb;
+extern crate shade_tree;
+
+use minifb::{Key, WindowOptions, Window};
+
+const WIDTH: usize = 1280;
+const HEIGHT: usize = 720;
+
+
+fn window_run() {
+    let buffer = shade_tree::blank_screen(WIDTH, HEIGHT);
+
+    let mut window = Window::new("Test - ESC to exit",
+                                 WIDTH,
+                                 HEIGHT,
+                                 WindowOptions::default()).unwrap_or_else(|e| {
+        panic!("Error: {}", e);
+    });
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        window.update_with_buffer(&buffer).unwrap();
+    }
+}
+
 fn main() {
-    println!("Hello, word!");
+    println!("Shade Tree");
+
+    window_run();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+pub fn blank_screen(width: usize, height: usize) -> Vec<u32> {
+    let mut buffer: Vec<u32> = vec![0; width * height];
+
+    for i in buffer.iter_mut() {
+        *i = 0xff_ffff;
+    }
+
+    buffer
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
