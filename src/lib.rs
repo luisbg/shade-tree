42118d991951d4af533ab93ec31966cb38449408
Luis de Bethencourt <luis@debethencourt.com>
2019-08-26 12:15:10 -0400

    Render: in a different thread

--- src/lib.rs
+++ src/lib.rs
@@ -10,9 +10,17 @@ extern crate rand;
 use camera::Camera;
 use rand::Rng;
 use sphere::Sphere;
+use std::sync::mpsc;
+use std::thread;
 use vec::Vec3f;
 use vec::Vec3i;
 use world::World;
+
+struct Pixel {
+    x: usize,
+    y: usize,
+    color: u32,
+}
 
 pub fn blank_screen(width: usize, height: usize) -> Vec<u32> {
     let mut buffer: Vec<u32> = vec![0; width * height];
@@ -44,7 +52,6 @@ pub fn gradient(width: usize, height: usize) -> Vec<u32> {
 }
 
 pub fn render(width: usize, height: usize, samples: usize) -> Vec<u32> {
-    let mut rng = rand::thread_rng();
     let mut buffer: Vec<u32> = vec![0; width * height];
 
     let origin = Vec3f::new(0.0, 0.0, 0.0);
@@ -58,22 +65,32 @@ pub fn render(width: usize, height: usize, samples: usize) -> Vec<u32> {
     world.add(Box::new(Sphere::new(Vec3f::new(-0.6, 0.3, -2.0), 0.3)));
     world.add(Box::new(Sphere::new(Vec3f::new(0.0, -100.5, -1.0), 100.0)));
 
-    for y in 1..height {
-        for x in 1..width {
-            let mut color = Vec3f::new(0.0, 0.0, 0.0);
-            for _s in 0..samples {
-                let u = (x as f64 + rng.gen_range(0.0, 1.0)) / width as f64;
-                let v = (y as f64 + rng.gen_range(0.0, 1.0)) / height as f64;
+    let (tx, rx) = mpsc::channel();
 
-                let p = camera::color(camera.get_ray(u, v), &world);
-                color = color + p;
+    thread::spawn(move || {
+        let mut rng = rand::thread_rng();
+
+        for y in 1..height {
+            for x in 1..width {
+                let mut color = Vec3f::new(0.0, 0.0, 0.0);
+                for _s in 0..samples {
+                    let u = (x as f64 + rng.gen_range(0.0, 1.0)) / width as f64;
+                    let v = (y as f64 + rng.gen_range(0.0, 1.0)) / height as f64;
+
+                    let p = camera::color(camera.get_ray(u, v), &world);
+                    color = color + p;
+                }
+                let color = Vec3i::new_from_f64(color / samples as f64);
+                let color = color.to_hex();
+
+                tx.send(Pixel { x, y, color }).unwrap();
             }
-            let color = Vec3i::new_from_f64(color / samples as f64);
-            let color = color.to_hex();
-
-            // y coordinate starts in the bottom, but in the buffer it starts in the top
-            buffer[((height - y) * width + x) as usize] = color;
         }
+    });
+
+    for pixel in rx {
+        // y coordinate starts in the bottom, but in the buffer it starts in the top
+        buffer[((height - pixel.y) * width + pixel.x) as usize] = pixel.color;
     }
 
     buffer
